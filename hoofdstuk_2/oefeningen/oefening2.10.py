5068be83eec2f61192bde468c8ebf8f62ff282c2
thicchead <metehan.karakoruk@gmail.com>
2022-09-22 10:58:19 +0200

    Update oefening2.10.py

--- hoofdstuk_2/oefeningen/oefening2.10.py
+++ hoofdstuk_2/oefeningen/oefening2.10.py
@@ -1,9 +1,9 @@
-var1 = 5
-var2 = 3
+var1 = int(input("Getal 1 = "))
+var2 = int(input("Getal 2 = "))
 
 var1 += var2
 var2 = var1 - var2
 var1 -= var2
 
-print(var1)
-print(var2)
+print("Getal 1: " + str(var1))
+print("Getal 2: " + str(var2))
